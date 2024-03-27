c47923d3ab5690d506f08bea16767b03658ad034
okamonster <okamonster0615@gmail.com>
2024-03-28 08:33:20 +0900

    feat:add DefaultHeader

---
+++ apps/ocha_dev/src/components/Navigations/DefaultHeader/index.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+import styles from './style.module.css'
+
+export const DefaultHeader = (): React.ReactNode => {
+  return (
+    <header className={styles.defaultHeader}>
+      <Link href="/">
+        <h1 className={styles.title}>OCHA.dev</h1>
+      </Link>
+    </header>
+  )
+}
