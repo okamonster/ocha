c5794da6f26a1183551c068998738e285225a12f
okamonster <okamonster0615@gmail.com>
2024-03-28 08:16:37 +0900

    feat:add DefaultLayout

--- apps/ocha_dev/src/pages/_app.tsx
+++ apps/ocha_dev/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app'
 
 import '~/styles/globals.css'
 import '~/styles/reset.css'
+import '~/styles/variables.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
