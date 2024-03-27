db59d5a52b4777863900af781ca7890f5a758a0a
okamonster <okamonster0615@gmail.com>
2024-03-28 07:39:51 +0900

    fix:chakraを剥がす

--- apps/ocha_dev/src/pages/_app.tsx
+++ apps/ocha_dev/src/pages/_app.tsx
@@ -1,17 +1,15 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import { ChakraProvider } from '@chakra-ui/react'
 import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 import '~/styles/globals.css'
+import '~/styles/reset.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
-      <ChakraProvider resetCSS>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <Component {...pageProps} />
     </MantineProvider>
   )
 }
