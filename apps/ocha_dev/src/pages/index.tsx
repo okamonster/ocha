b00ee9f13a044e4ce655ff03fe152d421905ab1d
okamonster <okamonster0615@gmail.com>
2024-03-28 11:50:49 +0900

    feat:add TopContainer

--- apps/ocha_dev/src/pages/index.tsx
+++ apps/ocha_dev/src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
 
+import { DefaultLayout } from '~/components/Layout/DefaultLayout'
+import { TopContainer } from '~/features/top/components/topContainer'
+
 const IndexPage: NextPage = () => {
-  return <div />
+  return (
+    <DefaultLayout>
+      <TopContainer />
+    </DefaultLayout>
+  )
 }
 
 export default IndexPage
