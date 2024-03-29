ebb60bfed42fabebd70921b27e29dbce13204c13
okamonster <okamonster0615@gmail.com>
2024-03-30 00:37:51 +0900

    feat:add blogPage

---
+++ apps/ocha_dev/src/features/blog/components/BlogContainer/index.tsx
@@ -0,0 +1,26 @@
+import styles from './style.module.css'
+
+import { SectionTitle } from '~/components/Base/SectionTitle'
+import { DefaultHeader } from '~/components/Navigations/DefaultHeader'
+import type { Blog } from '~/entitie/blog'
+import { BlogCard } from '~/features/top/components/blogCard'
+
+type Props = {
+  blogs: Array<Blog>
+}
+
+export const BlogContainer = ({ blogs }: Props): React.ReactNode => {
+  return (
+    <>
+      <DefaultHeader />
+      <main className={styles.blogContainer}>
+        <SectionTitle title="BLOG" description="記事一覧" />
+        <div className={styles.blogList}>
+          {blogs.map((blog) => (
+            <BlogCard blog={blog} key={blog.id} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
+}
--- apps/ocha_dev/src/pages/blog/index.tsx
+++ apps/ocha_dev/src/pages/blog/index.tsx
@@ -1,7 +1,39 @@
-import type { NextPage } from 'next'
+import type { InferGetStaticPropsType, NextPage } from 'next'
 
-const BlogPage: NextPage = () => {
-  return <div />
+import { DeafaultHead } from '~/components/Head/DefaultHead'
+import { DefaultLayout } from '~/components/Layout/DefaultLayout'
+import type { Blog } from '~/entitie/blog'
+import { BlogContainer } from '~/features/blog/components/BlogContainer'
+import { client } from '~/libs/client'
+
+type BlogPageProps = InferGetStaticPropsType<typeof getStaticProps>
+
+type Props = {
+  blogs: Array<Blog>
+}
+
+const BlogPage: NextPage<BlogPageProps> = ({ blogs }: Props) => {
+  return (
+    <DefaultLayout>
+      <DeafaultHead
+        title="お茶.dev"
+        description="お茶/岡本和輝のポートフォリオ"
+        ogpImage="https://www.xn--t8jy01w.dev/images/ogp.png"
+        path="blog"
+      />
+      <BlogContainer blogs={blogs} />
+    </DefaultLayout>
+  )
 }
 
 export default BlogPage
+
+export const getStaticProps = async () => {
+  const data = await client.get({
+    endpoint: 'blogs',
+  })
+
+  return {
+    props: { blogs: data.contents },
+  }
+}
