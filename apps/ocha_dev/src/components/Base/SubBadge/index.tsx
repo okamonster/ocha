a5bf83ed141ecdf451916c440deecafb5a4f8392
okamonster <okamonster0615@gmail.com>
2024-03-29 21:48:25 +0900

    feat:add subBadge

---
+++ apps/ocha_dev/src/components/Base/SubBadge/index.tsx
@@ -0,0 +1,13 @@
+import styles from './style.module.css'
+
+type Props = {
+  label: string
+}
+
+export const SubBadge = ({ label }: Props): React.ReactNode => {
+  return (
+    <div>
+      <span className={styles.subBadge}>{label}</span>
+    </div>
+  )
+}
