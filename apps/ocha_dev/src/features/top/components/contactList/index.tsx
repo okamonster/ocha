6b8cb54a7ec234df4e270dcc535d23d029699940
okamonster <okamonster0615@gmail.com>
2024-03-29 22:40:11 +0900

    fix:styles

--- apps/ocha_dev/src/features/top/components/contactList/index.tsx
+++ apps/ocha_dev/src/features/top/components/contactList/index.tsx
@@ -1,59 +1,60 @@
 import { ActionIcon } from '@mantine/core'
 import { FaInstagram, FaTwitter } from 'react-icons/fa'
 import { FaGithub, FaXTwitter } from 'react-icons/fa6'
-import classNames from 'classnames'
 import Link from 'next/link'
 
 import styles from './style.module.css'
 
-export const ContactList = (): React.ReactNode => {
-  const twitterCardClass = classNames(styles.contactCard, styles.twitter)
-  const instagramCardClass = classNames(styles.contactCard, styles.instagram)
-  const githubCardClass = classNames(styles.contactCard, styles.github)
+import { ContactCard } from '~/features/top/components/contactCard'
 
+export const ContactList = (): React.ReactNode => {
   return (
     <div className={styles.contactList}>
-      <div className={twitterCardClass}>
-        <p className={styles.title}>Twitter / X</p>
-        <div className={styles.iconList}>
-          <Link href="https://twitter.com/makura_nageru">
-            <ActionIcon variant="white" size={40} aria-label="Twitter">
-              <FaTwitter color="var(--color-twitter)" />
-            </ActionIcon>
-          </Link>
-          <Link href="https://twitter.com/makura_nageru">
-            <ActionIcon variant="white" size={40} aria-label="X">
-              <FaXTwitter color="var(--color-x)" />
-            </ActionIcon>
-          </Link>
-        </div>
-        <p className={styles.snsId}>@makura_nageru</p>
-      </div>
-
+      <ContactCard
+        title="Twitter / X"
+        contents={
+          <>
+            <Link href="https://twitter.com/makura_nageru">
+              <ActionIcon variant="white" size={40} aria-label="Twitter">
+                <FaTwitter color="var(--color-twitter)" />
+              </ActionIcon>
+            </Link>
+            <Link href="https://twitter.com/makura_nageru">
+              <ActionIcon variant="white" size={40} aria-label="X">
+                <FaXTwitter color="var(--color-x)" />
+              </ActionIcon>
+            </Link>
+          </>
+        }
+        snsId="@makura_nageru"
+        background="linear-gradient(to right, var(--color-twitter), #284859, #333)"
+      />
       <div className={styles.rightSection}>
-        <div className={instagramCardClass}>
-          <p className={styles.title}>Instagram</p>
-          <div className={styles.iconList}>
+        <ContactCard
+          title="Instagram"
+          contents={
             <Link href="https://www.instagram.com/oka_monster/">
               <ActionIcon variant="white" size={40} aria-label="Instagram">
                 <FaInstagram color="var(--color-instagram)" />
               </ActionIcon>
             </Link>
-          </div>
-          <p className={styles.snsId}>oka_monster</p>
-        </div>
+          }
+          snsId="oka_monster"
+          background="linear-gradient(45deg, #4c64d3, #cf2e92, #f26939, #ffdd83)"
+        />
 
-        <div className={githubCardClass}>
-          <p className={styles.title}>GitHub</p>
-          <div className={styles.iconList}>
+        <ContactCard
+          title="GitHub"
+          contents={
             <Link href="https://www.instagram.com/oka_monster/">
               <ActionIcon variant="white" size={40} aria-label="GitHub">
                 <FaGithub color="var(--color-github)" />
               </ActionIcon>
             </Link>
-          </div>
-          <p className={styles.snsId}>okamonster</p>
-        </div>
+          }
+          snsId="okamonster"
+          background="linear-gradient(to right, #000000, #434343)"
+        />
       </div>
     </div>
   )
