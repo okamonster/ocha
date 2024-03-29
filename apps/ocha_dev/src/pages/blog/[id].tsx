5c4495d06f5042124e895a844874f452cd6e4d12
okamonster <okamonster0615@gmail.com>
2024-03-30 02:12:45 +0900

    feat:add blog detail

--- apps/ocha_dev/src/pages/blog/[id].tsx
+++ apps/ocha_dev/src/pages/blog/[id].tsx
@@ -1,7 +1,45 @@
-import type { NextPage } from 'next'
+import type { InferGetStaticPropsType, NextPage } from 'next'
 
-const BlogDetailPage: NextPage = () => {
-  return <div />
+import { DeafaultHead } from '~/components/Head/DefaultHead'
+import { DefaultLayout } from '~/components/Layout/DefaultLayout'
+import type { Blog } from '~/entitie/blog'
+import { BlogDetailContainer } from '~/features/blog/components/BlogDetailContainer'
+import { client } from '~/libs/client'
+
+type BlogPageProps = InferGetStaticPropsType<typeof getStaticProps>
+
+type Props = {
+  id: string
+  blog: Blog
+}
+
+const BlogDetailPage: NextPage<BlogPageProps> = ({ id, blog }: Props) => {
+  return (
+    <DefaultLayout>
+      <DeafaultHead
+        title={blog.title}
+        description="お茶/岡本和輝のブログ"
+        ogpImage={blog.OGP.url}
+        path={`blog/${id}`}
+      />
+      <BlogDetailContainer blog={blog} />
+    </DefaultLayout>
+  )
 }
 
 export default BlogDetailPage
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'blogs' })
+
+  const paths = data.contents.map((content: Blog) => `/blog/${content.id}`)
+  return { paths, fallback: false }
+}
+
+export const getStaticProps = async (context: { params: { id: string } }) => {
+  const id = context.params.id
+  const data = await client.get({ endpoint: 'blogs', contentId: id })
+  return {
+    props: { id, blog: data },
+  }
+}
